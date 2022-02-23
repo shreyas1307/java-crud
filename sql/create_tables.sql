132864d070c9ede779ea2c9b2d8e58af29839ce4
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2022-02-23 11:18:18 +0530

    initialised repository

---
+++ sql/create_tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS book (
+    student_id INT NOT NULL,
+    name varchar(250) NOT NULL,
+    email varchar(250) NOT NULL
+    dob DATE NOT NULL,
+    PRIMARY KEY(student_id)
+);
+
+
+
+
+
